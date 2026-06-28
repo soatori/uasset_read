83bbf72d6f2be35e804f4a904e1df5e5b2397184
soatori <30325355+soatori@users.noreply.github.com>
2026-06-28 15:47:56 +0800

    feat: 新增 DataTable 资产类型处理器 (#194)

--- src/uasset_read/parsers/asset_types/__init__.py
+++ src/uasset_read/parsers/asset_types/__init__.py
@@ -213,6 +213,7 @@ def register_asset_type_handlers() -> None:
         ("sound_wave", "parse_sound_wave", ["SoundWave"], "SoundWaveHandler"),
         ("sound_attenuation", "parse_sound_attenuation", ["SoundAttenuation"], "SoundAttenuationHandler"),
         ("anim_data_model", "parse_anim_data_model", ["AnimationDataModel"], "AnimDataModelHandler"),
+        ("data_table", "parse_data_table", ["DataTable"], "DataTableHandler"),
     ]
     for module, func_name, class_names, handler_name in _optional:
         try:
