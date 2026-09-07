1204c0425b66232fcd1d416bc971519dc022fd1a
soatori <cbsjz@qq.com>
2026-09-07 22:51:34 +0800

    refactor: drop PACKAGE_TRAILER_FOOTER_TAG orphan and its self-referential test

--- src/uasset_read/constants.py
+++ src/uasset_read/constants.py
@@ -25,7 +25,6 @@ PACKAGE_FILE_TAG_SWAPPED = 0xC1832A9E  # Swapped byte order magic tag
 
 # PackageTrailer tags (UE5)
 PACKAGE_TRAILER_HEADER_TAG = 0xD1C43B2E80A5F697
-PACKAGE_TRAILER_FOOTER_TAG = 0x29BFCA045138DE76
 
 # ============================================================================
 # Version constants
--- tests/serialization/test_package_trailer.py
+++ tests/serialization/test_package_trailer.py
@@ -2,7 +2,6 @@
 import pytest
 from uasset_read.constants import (
     PACKAGE_TRAILER_HEADER_TAG,
-    PACKAGE_TRAILER_FOOTER_TAG,
     PACKAGE_FILE_TAG,
     UE5_PAYLOAD_TOC,
     UE5_DATA_RESOURCES,
@@ -11,10 +10,6 @@ from uasset_read.constants import (
 
 def test_header_tag_value():
     assert PACKAGE_TRAILER_HEADER_TAG == 0xD1C43B2E80A5F697
-
-
-def test_footer_tag_value():
-    assert PACKAGE_TRAILER_FOOTER_TAG == 0x29BFCA045138DE76
 
 
 def test_payload_toc_version():
